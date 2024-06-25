8e84ec6bc40c042ec224baa1bab788bf4d6f856e
Jean-Pierre <jean-pierre.chevallet@univ-grenoble-alpes.fr>
2024-06-25 17:01:08 +0200

    Lecture de la grammaire : début

--- test/test_param.py
+++ test/test_param.py
@@ -12,12 +12,23 @@
 #    __init__(self,id,):
 class SyntaxNode:
     def __init__(self,id,name):
-        self.id = id
+        self.id = id # Identifiand du noeud, quelques lettres en majuscule
         self.name = name
-        self.next_level = []
+        self.nodes = [] # Noeuds fils
+
+    # Décode la description d'un noeud et crée un nouveau noeud
+    @staticmethod
+    def new(line):
+        line = line.lstrip()
+        content = line.split(':')
+        return SyntaxNode(content[0],content[1])
+
+    # Ajoute un noeud fils
+    def append(self,node):
+        self.nodes.append(node)
 
     def __repr__(self) -> str:
-        return '{'+self.id+','+self.name+','+str(self.next_level)+'}'
+        return '{'+self.id+','+self.name+','+str(self.nodes)+'}'
 
 
 class Syntax:
@@ -44,10 +55,25 @@ class Syntax:
                     else:
                         # Calcule le niveau par comptage du nombre d'espaces
                         level = int(level / space_per_level)
+                # Si on retourne au niveau 0 on redémmage avec un nouveau noeud racine
                 if level == 0:
                     # Nouveau noeud de niveau zéro
-                    node = SyntaxNode('TST',"Test")
+                    node = SyntaxNode.new(line)
                     self.nodes.append(node)
+                    current_node = node
+                    current_level = 0
+                else:
+                    if level == current_level:
+                        # Ajoute un noeud au noeud courant
+                        current_node.append(SyntaxNode.new(line))
+                    elif level == current_level + 1:
+                        # Descent d'un niveau 
+                        node = SyntaxNode.new(line)
+                        # Ajoute ce nouveau noeud au noeud courant
+                        current_node.append(node)
+                        # met à jour le suivit
+                        current_level = level
+                        current_node = node
                 #print(level,line)
 
 syntax = Syntax("dx7_meaning.txt")
